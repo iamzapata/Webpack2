0b68d505d25edac795a1e8236c9685ca7a6708dc
Andres Zapata <me@azapata.io>
2018-01-28 22:21:37 -0500

    Add babel, css loaders and ExtractTextPlugin

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const ExtractTextPlugin  = require('extract-text-webpack-plugin')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
+}
+
+
+module.exports = config
